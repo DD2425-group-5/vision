b938a31e192604e0e5f23546ea717240cdfb5828
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-23 00:00:51 +0200

    Basic thresholding on single test image passed as argument

--- object_lib/src/imageExtraction.cpp
+++ object_lib/src/imageExtraction.cpp
@@ -1,15 +1,56 @@
 #include <ros/ros.h>
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv/highgui.h>
 #include <iostream>
 
+using namespace cv;
 
 
+void showImage(std::string fileName) {
+    Mat im;
+    
+    
+    im = imread(fileName, CV_LOAD_IMAGE_COLOR);
 
+    if (!im.data) {
+	ROS_INFO("Could not load image %s", fileName.c_str());
+    }
+    
+    Mat img;
+    resize(im, img, Size(), 0.2, 0.2);
+    
+    namedWindow("Normal", WINDOW_AUTOSIZE);
+    imshow("Normal", img);
+
+    Mat gray;
+    cvtColor(img, gray, CV_RGB2GRAY);
+
+    namedWindow("Gray", WINDOW_AUTOSIZE);
+    imshow("Gray", gray);
+
+    Mat thresh;
+    threshold(gray, thresh, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+    namedWindow("Thresh", WINDOW_AUTOSIZE);
+    imshow("Thresh", thresh);
+
+    imwrite("thresholded.jpg", thresh);
+    
+    Mat masked;
+    img.copyTo(masked, thresh);
+    
+    namedWindow("mask", WINDOW_AUTOSIZE);
+    imshow("mask", masked);
+
+    imwrite("mask.jpg", masked);
+    
+    waitKey(0);
+}
 
 int main(int argc, char *argv[]) {
-     for (int i = 0; i < argc; i++) {
-	  ROS_INFO("Arg %d: %s", i, argv[i]);
-     }
-
+    for (int i = 0; i < argc; i++) {
+	ROS_INFO("Arg %d: %s", i, argv[i]);
+    }
+    showImage(argv[1]);
 }
