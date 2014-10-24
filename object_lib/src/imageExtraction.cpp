db25134b762b20262783ab7070c62fea0ac58940
Michal Staniaszek <m.staniaszek@gmail.com>
2014-10-24 13:45:33 +0200

    Added support for single directory processing
    Added some utility functions to reduce code reuse.

--- object_lib/src/imageExtraction.cpp
+++ object_lib/src/imageExtraction.cpp
@@ -1,40 +1,7 @@
 #include "imageExtraction.hpp"
 
 using namespace cv;
-
-
-bool isType(std::string path, mode_t mode) {
-    struct stat s;
-    if (stat(path.c_str(), &s) == 0) {
-	if (s.st_mode & mode) {
-	    return true;
-	} else {
-	    return false;
-	}
-    } else {
-	return false;
-    }
-}
-
-bool isDir(std::string path) {
-    return isType(path, S_IFDIR);
-}
-
-bool isFile(std::string path) {
-    return isType(path, S_IFREG);
-}
-
-/**
- * Insert a suffix at the end of a filename which has an extension. The suffix
- * will be placed before the extension.
- */
-std::string insertSuffix(std::string fileName, std::string suffix) {
-    int dotInd = fileName.find_last_of(".");
-    std::string raw = fileName.substr(0, dotInd);
-    std::string ext = fileName.substr(dotInd);
-    
-    return raw + suffix + ext;
-}
+using namespace std;
 
 /**
  * Main function to extract objects from images. Gets a list of files to
@@ -42,23 +9,23 @@ std::string insertSuffix(std::string fileName, std::string suffix) {
  * images are then placed in the output directory with the directory names of
  * the original locations preserved.
  */
-void extractObjects(std::string inputDir, std::string outputDir) {
+void extractObjects(string inputDir, string outputDir) {
     if (!isDir(inputDir)) {
 	ROS_INFO("Input directory %s is not a directory.", inputDir.c_str());
-	std::exit(1);
+	exit(1);
     }
 
     if (isDir(outputDir)) {
 	ROS_INFO("%s already exists. Write to this directory? (y/n)", outputDir.c_str());
-	std::string reply;
+	string reply;
 	bool done = false;
 	while (!done) {
-	    std::cin >> reply;
+	    cin >> reply;
 	    if (reply.compare("y") == 0 || reply.compare("Y") == 0) {
 		done = true;
 	    } else if (reply.compare("n") == 0 || reply.compare("N") == 0) {
 		ROS_INFO("OK, exiting.");
-		std::exit(0);
+		exit(0);
 	    } else {
 		ROS_INFO("Please reply with y or n.");
 	    }
@@ -68,47 +35,66 @@ void extractObjects(std::string inputDir, std::string outputDir) {
 	mkdir(outputDir.c_str(), S_IRWXU | S_IRWXG | S_IROTH);
     }
 
-    std::map<std::string, std::vector<std::string> > dirMap = getFilesToProcess(inputDir);
-    std::map<std::string, std::vector<std::string> >::iterator mapIterator;
-    std::vector<std::string>::iterator fileIterator;
+    map<string, vector<string> > dirMap = getFilesToProcess(inputDir);
+    
+    map<string, vector<string> >::iterator mapIterator;
+    vector<string>::iterator fileIterator;
     // Go over each directory
     for (mapIterator = dirMap.begin(); mapIterator != dirMap.end(); ++mapIterator) {
+	string dir = mapIterator->first;
+	string outputPath = dir;
+	vector<string> files = mapIterator->second;
 
-	std::string dir = mapIterator->first;
-	std::vector<std::string> files = mapIterator->second;
 	// make the directory into which the files will be placed, opencv
-	// doesn't create directories.
-	std::string dirPath = std::string(outputDir + "/" + dir);
-	if (!isDir(dirPath)) {
-	    std::cout << "Creating directory " << dirPath << std::endl;
-	    mkdir(dirPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH);
+	// doesn't create directories. Remove the final slash from the output
+	if (outputPath.compare("") != 0) {
+	    outputPath.replace(0, inputDir.size(), outputDir);
+	} else {
+	    outputPath.replace(0, inputDir.size(), outputDir + removeBaseName(inputDir));
+	    dir = inputDir;
 	}
-	std::cout << "Processing directory " << dir << "..." << std::endl;
+	std::cout << outputPath << std::endl;
+	if (!isDir(outputPath)) {
+	    cout << "Creating directory " << outputPath << endl;
+	    mkdir(outputPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH);
+	}
+	cout << "Processing directory " << dir << "..." << endl;
 	// For each file in the current directory
 	for (fileIterator = files.begin(); fileIterator != files.end(); ++fileIterator){
 	    // Extract the blob and then write it to the output directory,
-	    // preserving its original path. Need to reconstruct the sub-path
-	    // of the input image.
-	    Mat extract = detectBlob(inputDir + dir + "/" + *fileIterator);
-	    imwrite(outputDir + "/" + dir + "/" + insertSuffix(*fileIterator, "_object"), 
-		    extract);
+	    // preserving its original path below the input directory.
+	    // cout << "Processing " << *fileIterator << endl;
+	    // cout << "Will output to " << insertSuffix(outputPath + removeBaseName(*fileIterator), "_object") << endl;
+	    // Replace the input directory with the output directory in the filename
+	    Mat extract = threshContour(*fileIterator);
+	    imwrite(insertSuffix(outputPath + removeBaseName(*fileIterator), "_object"), extract);
 	    // \r at the beginning of the line returns to the beginning, using
 	    // flush at the end insted of endl allows overwriting the stuff
 	    // written on the current line. Do this to prevent lots of lines of
 	    // output.
-	    std::cout << "\rProcessed " << fileIterator - files.begin() + 1
-		      << " of " << files.end() - files.begin() 
-		      << " images." << std::flush;
+	    cout << "\rProcessed " << fileIterator - files.begin() + 1
+	    	      << " of " << files.end() - files.begin() 
+	    	      << " images." << flush;
 	}
-	std::cout << std::endl;
+	cout << endl;
     }
 }
 
-bool contourSort(std::vector<Point> c1, std::vector<Point> c2) {
+bool contourSort(vector<Point> c1, vector<Point> c2) {
     return contourArea(c1) > contourArea(c2);
 }
 
-Mat detectBlob(std::string fileName) {
+// int threshSet;
+// int maxThresh = 255;
+// Mat grayImg;
+
+// void threshCallback(int, void*) {
+//     Mat thresh;
+//     threshold(grayImg, thresh, threshSet, 255, THRESH_BINARY_INV);
+//     imshow("source", thresh);
+// }
+
+Mat threshContour(string fileName) {
     Mat im;
     
     im = imread(fileName, CV_LOAD_IMAGE_COLOR);
@@ -121,19 +107,38 @@ Mat detectBlob(std::string fileName) {
     resize(im, img, Size(), 0.2, 0.2);
     Mat gray;
     cvtColor(img, gray, CV_RGB2GRAY);
+
+    // namedWindow("source", CV_WINDOW_AUTOSIZE);
+    // imshow("source", grayImg);
+    // createTrackbar("Threshold", "source", &threshSet, maxThresh, threshCallback);
+    // threshCallback(0, 0);
+    // waitKey(0);
+
     Mat thresh;
     threshold(gray, thresh, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+//    threshold(gray, thresh, threshSet, 255, THRESH_BINARY_INV); 
     
-    std::vector<std::vector<Point> > contours;
-    std::vector<Vec4i> hierarchy;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
     
     findContours(thresh, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    std::sort(contours.begin(), contours.end(), contourSort);
+    sort(contours.begin(), contours.end(), contourSort);
 
     Mat drawing = Mat::zeros(img.size(), CV_8UC1);
     Scalar colour = Scalar(1,1,1);
     drawContours(drawing, contours, 0, colour, CV_FILLED);
+
+    //  RNG rng;    
+    // Mat cont;
+    // img.copyTo(cont);
+    // for (int i = 0; i < contours.size(); i++) {
+    // 	Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+    // 	drawContours(cont, contours, i, color, 2, 8, hierarchy, 0, Point());
+    // }
+    // namedWindow("cont", CV_WINDOW_AUTOSIZE);
+    // imshow("cont", cont);
+    // waitKey(0);
     
     Mat extracted;
     img.copyTo(extracted, drawing);
@@ -141,61 +146,28 @@ Mat detectBlob(std::string fileName) {
     return extracted;
 }
 
-std::vector<std::string> getDirFiles(std::string dirName) {
-    DIR* dir;
-    struct dirent* ent;
-    std::vector<std::string> fileList;
-    // Go through the directory and put all the directories in it into a list. These
-    // are assumed to be the directories containing images to be processed
-    if ((dir = opendir(dirName.c_str())) != NULL) {
-	while ((ent = readdir(dir)) != NULL) {
-	    // not interested in the current or parent dirs
-	    if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0){
-		continue;
-	    }
-	    fileList.push_back(std::string(ent->d_name));
-	}
-	closedir(dir);
-    } else {
-	// Couldn't access directory
-	ROS_INFO("Error processing %s:", dirName.c_str());
-	perror("");
-    }
+map<string, vector<string> > getFilesToProcess(string dirName) {
+    map<string, vector<string> > dirFileMap;
+
+    DirContents top = listDir(dirName);
     
-    return fileList;
-}
-
-/**
- * Mixing c and c++ is bad, but unavoidable here.
- */ 
-std::map<std::string, std::vector<std::string> > getFilesToProcess(std::string dirName) {
-
-
-    std::map<std::string, std::vector<std::string> > dirFileMap;
-    DIR* dir;
-    struct dirent* ent;
-    // Go through the directory and put all the directories in it into a list. These
-    // are assumed to be the directories containing images to be processed
-    if ((dir = opendir(dirName.c_str())) != NULL) {
-	while ((ent = readdir(dir)) != NULL) {
-	    // not interested in the current or parent dirs, or files in this dir
-	    if (isFile(dirName + std::string(ent->d_name)) 
-		|| strcmp(ent->d_name, ".") == 0 
-		|| strcmp(ent->d_name, "..") == 0){
-		continue;
-	    }
-	    std::string subdName = std::string(ent->d_name);
-	    // Insert the directory and its files into the map to be processed. Don't need the
-	    // full path in the map, reconstruct it later.
-	    dirFileMap.insert(std::pair<std::string, std::vector<std::string> >(subdName, getDirFiles(dirName + subdName)));
+    if (top.dirs.size() == 0) {
+	// If there are no directories, assume you want to process files in the
+	// directory
+	for (int i = 0; i < top.files.size(); i++){
+	    dirFileMap.insert(pair<string, vector<string> >("", top.files));
 	}
-	closedir(dir);
     } else {
-	// Couldn't access directory
-	perror("");
-	ROS_INFO("Error processing %s", dirName.c_str());
+	// Otherwise, assume that the maximum depth of the directory structure
+	// is 1, and extract the file names out of the directories below the one
+	// passed as a parameter.
+	for (int dir = 0; dir < top.dirs.size(); dir++){
+	    DirContents sub = listDir(top.dirs[dir]);
+	    if (sub.files.size() != 0) {
+		dirFileMap.insert(pair<string, vector<string> >(top.dirs[dir], sub.files));
+	    }
+	}
     }
-
     return dirFileMap;
 }
 
--- object_lib/src/imageExtraction.hpp
+++ object_lib/src/imageExtraction.hpp
@@ -1,20 +1,114 @@
 #include <ros/ros.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv/highgui.h>
 #include <iostream>
 #include <algorithm>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string>
+#include <iostream>
 #include <map>
 
-bool isType(std::string path, mode_t mode);
-bool isDir(std::string path);
-bool isFile(std::string path);
+struct DirContents {
+    std::string path;
+    std::vector<std::string> dirs;
+    std::vector<std::string> files;
+};
+
+bool isType(std::string path, mode_t mode) {
+    struct stat s;
+    if (stat(path.c_str(), &s) == 0) {
+	if (s.st_mode & mode) {
+	    return true;
+	} else {
+	    return false;
+	}
+    } else {
+	return false;
+    }
+}
+
+bool isDir(std::string path) {
+    return isType(path, S_IFDIR);
+}
+
+bool isFile(std::string path) {
+    return isType(path, S_IFREG);
+}
+
+/**
+ * Removes the trailing slash from a directory path if there is one.
+ */
+std::string cleanDirPath(std::string path) {
+    std::string dir = path;
+    if (path[path.size() - 1] == '/'){
+	dir = path.substr(0, dir.size() - 1);
+    }
+    return dir;
+}
+
+/**
+ * Removes the base name from a path, leaving either the directory or the 
+ * file name.
+ */
+std::string removeBaseName(std::string path) {
+    if (isFile(path)){
+	int lastDir = path.find_last_of("/");
+	return path.substr(lastDir);
+    } else if (isDir(path)){
+	std::string cleanPath = cleanDirPath(path);
+	int lastDir = cleanPath.find_last_of("/");
+	return cleanPath.substr(lastDir + 1);
+    }
+    return path;
+}
+
+/**
+ * Insert a suffix at the end of a filename which has an extension. The suffix
+ * will be placed before the extension.
+ */
+std::string insertSuffix(std::string fileName, std::string suffix) {
+    int dotInd = fileName.find_last_of(".");
+    std::string raw = fileName.substr(0, dotInd);
+    std::string ext = fileName.substr(dotInd);
+    
+    return raw + suffix + ext;
+}
+
+
+DirContents listDir(std::string path) {
+    DirContents c;
+    c.path = cleanDirPath(path);
+    DIR* dir;
+    struct dirent* ent;
+    if ((dir = opendir(path.c_str())) != NULL) {
+	while ((ent = readdir(dir)) != NULL) {
+	    // ignore current and parent dirs
+	    if (strcmp(ent->d_name, ".") == 0 
+		|| strcmp(ent->d_name, "..") == 0){
+		continue;
+	    }
+	    std::string fullPath = c.path + "/" + std::string(ent->d_name);
+	    if (isFile(fullPath)) {
+		c.files.push_back(fullPath);
+	    } else if (isDir(fullPath)) {
+		c.dirs.push_back(fullPath);
+	    }
+	}
+    } else {
+	// Couldn't access directory
+	perror("");
+	ROS_INFO("Error processing %s", path.c_str());
+    }
+
+    return c;
+}
+
+
 void extractObjects(std::string inputDir, std::string outputDir);
 bool contourSort(std::vector<cv::Point> c1, std::vector<cv::Point> c2);
-cv::Mat detectBlob(std::string fileName);
-std::vector<std::string> getDirFiles(std::string dirName);
+cv::Mat threshContour(std::string fileName);
 std::map<std::string, std::vector<std::string> > getFilesToProcess(std::string dirName);
 std::string insertSuffix(std::string fileName, std::string suffix);
