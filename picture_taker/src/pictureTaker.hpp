c57dd97f89235236979ea23adc7b52b497c7e8d1
Michal Staniaszek <m.staniaszek@gmail.com>
2014-11-04 15:27:32 +0100

    Added some switches to pictureTaker, filenames improved
    Can now save images from the stream without pressing buttons
    Output directory and topic can be specified
    Can display image stream while program is running
    Filenames now use the current date and time

--- picture_taker/src/pictureTaker.hpp
+++ picture_taker/src/pictureTaker.hpp
@@ -11,24 +11,36 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
+#include <sysutil/sysutil.hpp>
 #include <cstdlib>
 #include <sstream>
 #include <time.h>
 #include <stdlib.h>
+#include <string>
 //#include <opencv/highgui.h>
+
+
 
 class PictureTakerNode {
 public:
+    static const std::string help;
 
     ros::Subscriber rgb_subscriber;
 
     void update();
-    PictureTakerNode(int argc, char* argv[]);
+    PictureTakerNode(int argc, char* argv[], std::string outDir, std::string topic, bool display, bool stream, int streamRate);
+
 
 private:
     ros::Time t_camera;
     sensor_msgs::Image::ConstPtr img;
     bool rgb;
+    bool stream;
+    bool display;
+    int streamRate;
+    std::string outDir;
+    std::string topic;
+    time_t rawTime;
 
     void bgr2rgb(cv_bridge::CvImagePtr cv_ptr);
     void rgbCallback(const sensor_msgs::Image::ConstPtr &msg);
@@ -36,8 +48,7 @@ private:
 
     cv_bridge::CvImagePtr convertImage();
 
-    ros::NodeHandle nodeSetup(int argc, char* argv[]);
-    void runNode(ros::NodeHandle handle);
+    void runNode();
 };
 
 #endif // PICTURETAKER_HPP
