37b464f819e3744e25884175173b7090efff52eb
Michal Staniaszek <m.staniaszek@gmail.com>
2014-11-11 12:24:11 +0100

    Moved some standard functionality to the utilities package

--- pcl_methods/src/plane_extraction.cpp
+++ pcl_methods/src/plane_extraction.cpp
@@ -7,6 +7,8 @@ ros::Publisher plane;
 ros::Publisher exobj;
 ros::Publisher bbox;
 
+using namespace PCLUtil;
+
 /**
  * Subscribing to a PCL type will automatically convert the ROS message type.
  * Need to use a Ptr here, because the setInputCloud requires a ConstPtr. If you
@@ -26,10 +28,11 @@ void pcl_callback(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& msg){
     // get the bounds of the plane
     CloudBounds<pcl::PointXYZRGB> domBounds = getCloudBounds<pcl::PointXYZRGB>(domPlane);
     printBounds(domBounds);
-    bbox.publish(boundBoxPointCloud(domBounds));
+    CloudBounds<pcl::PointXYZRGB> scaledBounds = scaleBounds(domBounds, 0.8);
+    bbox.publish(boundBoxPointCloud(scaledBounds));
 
     // Extract points in the remaining cloud which are above or below the plane
-    pcl::PointCloud<pcl::PointXYZRGB> onPlanePoints = getPointsInBounds(domBounds, msg, coefficients);
+    pcl::PointCloud<pcl::PointXYZRGB> onPlanePoints = getPointsInBounds(scaledBounds, msg, coefficients);
 
     exobj.publish(onPlanePoints);
 }
@@ -117,90 +120,6 @@ pcl::PointCloud<pcl::PointXYZRGB> getPointsInBounds(
     return pcl::PointCloud<pcl::PointXYZRGB>(*cloud, pointIndices);
 }
 
-bool pointInBounds(pcl::PointXYZRGB point, CloudBounds<pcl::PointXYZRGB> bounds) {
-    // short circuit evaluation
-    if (point.x < bounds.lower.x || point.x > bounds.upper.x
-	|| point.y < bounds.lower.y || point.y > bounds.upper.y
-	|| point.z < bounds.lower.z || point.z > bounds.upper.z){
-	return false;
-    }
-    return true;
-}
-
-void projTest()
-{
-    std::cout << "start" << std::endl;
-    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-    coefficients->values.resize (4);
-    std::cout << "cloud" << std::endl;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
-    // plane at 10 on the y axis, parallel to XZ plane
-    coefficients->values[0] = -0;
-    coefficients->values[1] = 1;
-    coefficients->values[2] = 0;
-    coefficients->values[3] = -10;
-
-
-    std::cout << "values" << std::endl;
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    
-    cloud->width  = 3;
-    cloud->height = 1;
-    std::cout << "resize" << std::endl;
-    cloud->points.resize (cloud->width * cloud->height);
-    // expect these points to have y coord of 10 after the projection
-    cloud->points[0].x = 1;
-    cloud->points[0].y = 2;
-    cloud->points[0].z = 3;
-
-    cloud->points[1].x = 2;
-    cloud->points[1].y = 0;
-    cloud->points[1].z = 1;
-
-    cloud->points[2].x = 10;
-    cloud->points[2].y = 20;
-    cloud->points[2].z = -5;    
-
-    std::cerr << "Cloud before projection: " << std::endl;
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-	std::cerr << "    " << cloud->points[i].x << " " 
-		  << cloud->points[i].y << " " 
-		  << cloud->points[i].z << std::endl;
-
-    pcl::ProjectInliers<pcl::PointXYZ> proj;
-    proj.setModelType (pcl::SACMODEL_PLANE);
-    proj.setInputCloud (cloud);
-    proj.setModelCoefficients(coefficients);
-    proj.filter(*cloud_projected);
-
-    std::cerr << "Cloud after projection: " << std::endl;
-    for (size_t i = 0; i < cloud_projected->points.size (); ++i)
-	std::cerr << "    " << cloud_projected->points[i].x << " " 
-		  << cloud_projected->points[i].y << " " 
-		  << cloud_projected->points[i].z << std::endl;
-}
-
-template <typename T>
-CloudBounds<T> getCloudBounds(const typename pcl::PointCloud<T>::ConstPtr& cloud){
-    T min;
-    T max;
-    pcl::getMinMax3D(*cloud, min, max);
-    return CloudBounds<T>(min, max);
-}
-
-pcl::PointXYZRGB initXYZRGB(float x, float y, float z, int r, int g, int b){
-    pcl::PointXYZRGB p;
-    p.x = x;
-    p.y = y;
-    p.z = z;
-    p.r = r;
-    p.g = g;
-    p.b = b;
-    
-    return p;
-}
-
 /**
  * Create a point cloud containing the 8 points which represent the bounding box given
  */
@@ -220,86 +139,6 @@ pcl::PointCloud<pcl::PointXYZRGB> boundBoxPointCloud(CloudBounds<pcl::PointXYZRG
 
     return bbox;
 }
-
-/**
- * Extract planes which take up >= the given proportion of points in the
- * original cloud. Does multiple passes on the cloud until all planes are
- * removed or labelled. There is no upper limit on the size of the plane
- * that can be extracted.
- * !!This function modifies the object that it is given!!
- *
- * If a colour is provided as an argument, then RGB values for the points in
- * planes are set to that value. Otherwise, the points are removed from the
- * plane. A colour of (-1, -1, -1) will also result in point erasure. 
- *
- * The returned vector contains the planes in descending order of number of
- * points.
- */
-// std::vector<pcl::PointCloud<pcl::PointXYZRGB> > extractPlanesByProportion(
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
-//     float proportion,
-//     float tolerance,
-//     Colour colour) {
-
-//     std::vector<pcl::PointCloud<pcl::PointXYZRGB> > extractedPlanes;
-//     // Set a boolean if we are to erase 
-//     bool erase;
-//     if (colour.r == -1 && colour.b == -1 && colour.g == -1){
-// 	erase = true;
-//     }
-    
-//     int nExtractedPlanes = 0;
-//     int minPointsInPlane = (int) proportion * cloud->points.size();
-
-    
-//     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
-//     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-//     pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-    
-//     // Create the segmentation object
-//     pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-//     // Optional
-//     seg.setOptimizeCoefficients(true);
-//     // Mandatory
-//     seg.setModelType(pcl::SACMODEL_PLANE);
-//     seg.setMethodType(pcl::SAC_RANSAC);
-//     seg.setDistanceThreshold(tolerance);
-
-//     seg.setInputCloud(cloud);
-//     seg.segment(*inliers, *coefficients);
-
-//     int nPointsInPlane = inliers->indices.size();
-//     std::cerr << "Model #inliers: " << nPointsInPlane << std::endl;
-//     if (nPointsInPlane == 0) {
-// 	// Couldn't find a plane, so return
-//     	PCL_ERROR("Could not estimate a planar model for the given dataset.");
-// 	return extractedPlanes;
-//     } else if (nPointsInPlane < minPointsInPlane) {
-// 	// found a plane, but smaller than minimum proportion, so no point
-// 	// continuing.
-// 	PCL_INFO("Extracted plane is smaller than minimum proportion.");
-// 	return extractedPlanes;
-//     }
-//     std::cout << "Modifying cloud" << std::endl;
-
-//     extract.setInputCloud(cloud);
-//     extract.setIndices(inliers);
-//     extract.setNegative(true); // return points which are NOT the indices
-//     extract.filter(*plane);
-
-//     if (erase) {
-// 	// the cloud now corresponds to all points that were not in the plane.
-// 	*cloud = *plane; // using the swap function doesn't work for some reason
-//     } else { // Colour all the inliers with the requested colour
-// 	PCL_INFO("Colouring points");
-// 	for (size_t i = 0; i < inliers->indices.size(); ++i) {
-// 	    cloud->points[inliers->indices[i]].r = colour.r;
-// 	    cloud->points[inliers->indices[i]].g = colour.g;
-// 	    cloud->points[inliers->indices[i]].b = colour.b;
-// 	}
-//     }
-//     return extractedPlanes;
-// }
 
 int main (int argc, char* argv[]) {
     ros::init(argc, argv, "pcl_test");
--- pcl_methods/src/plane_extraction.hpp
+++ pcl_methods/src/plane_extraction.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ros/ros.h>
+#include <pclutil/pclutil.hpp>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/io/pcd_io.h>
@@ -8,52 +9,20 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/common/common.h>
-#include <pcl/cloud_iterator.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/project_inliers.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <string>
 
-struct Colour {
-    Colour(int rr, int gg, int bb) : r(rr), g(gg), b(bb){};
-    int r;
-    int g;
-    int b;
-};
-
-template <typename T>
-struct CloudBounds {
-    CloudBounds(T l, T u) : lower(l), upper(u) {}
-    T lower;
-    T upper;
-};
-
-void printBounds(CloudBounds<pcl::PointXYZRGB> bounds){
-    std::cout << bounds.lower.x << " <= x <= " << bounds.upper.x << std::endl;
-    std::cout << bounds.lower.y << " <= y <= " << bounds.upper.y << std::endl;
-    std::cout << bounds.lower.z << " <= z <= " << bounds.upper.z << std::endl;
-}
-
-pcl::PointCloud<pcl::PointXYZRGB> boundBoxPointCloud(CloudBounds<pcl::PointXYZRGB> bounds);
-
 void pcl_callback(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& msg);
-
-// std::vector<pcl::PointCloud<pcl::PointXYZRGB> > extractPlanesByProportion(
-//     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, 
-//     float proportion,
-//     float tolerance = 0.05,
-//     Colour colour = Colour(-1,-1,-1));
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr extractDominantPlane(
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
     pcl::ModelCoefficients::Ptr coefficients,
     float tolerance);
 
-template <typename T>
-CloudBounds<T> getCloudBounds(const typename pcl::PointCloud<T>::ConstPtr& cloud);
-bool pointInBounds(pcl::PointXYZRGB point, CloudBounds<pcl::PointXYZRGB> bounds);
 pcl::PointCloud<pcl::PointXYZRGB> getPointsInBounds(
-    CloudBounds<pcl::PointXYZRGB> bounds,
+    PCLUtil::CloudBounds<pcl::PointXYZRGB> bounds,
     const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud,
     const pcl::ModelCoefficients::Ptr& coefficients);
 
+pcl::PointCloud<pcl::PointXYZRGB> boundBoxPointCloud(PCLUtil::CloudBounds<pcl::PointXYZRGB> bounds);
